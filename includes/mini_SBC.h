8a79529b3a18440d180a390e6f316f2f6bb89534
TryLessFold <artem_141999@mail.ru>
2019-10-18 18:08:43 +0700

    Save point 18.10.2019

--- includes/mini_SBC.h
+++ includes/mini_SBC.h
@@ -10,33 +10,29 @@
 #include <rx_get_port.h>
 #include <px_SBC.h>
 
-#define FIRST_ADDR_FROMTO "192.168.23.134"
-#define FIRST_PORT 8000
-#define FIRST_PORT_TEXT ":8000"
-#define FIRST_AUDIO_PORT 8010
-#define FIRST_AUDIO_PORT_TEXT ":8010"
-
-#define SECOND_ADDR_FROMTO "192.168.23.134"
-#define SECOND_PORT 9000
-#define SECOND_PORT_TEXT ":9000"
-#define SECOND_AUDIO_PORT 9010
-#define SECOND_AUDIO_PORT_TEXT ":9010"
+#define MAX_PORTS 16
 
 typedef struct my_config
 {
+    pj_sockaddr local_addr;
+    unsigned short port[MAX_PORTS];
+    
     pjsip_endpoint *sip_endpt;
     pjmedia_endpt *media_endpt;
     pjsip_transport *trans_port[2];
     pjmedia_transport *mtrans_port[2];
-    pjsip_dialog *dlg[2];
     pj_caching_pool cp;
-    pj_thread_t *thread;
     pj_pool_t *pool;
     pjsip_tpmgr *tpmgr;
+   
+    pj_thread_t *thread;
+    
+    pjsip_dialog *dlg[2];
     unsigned networks_cnt;
-    pjsip_host_port networks[16];
-    unsigned short ports[2];
+    pjsip_host_port[MAX_PORTS];
 } my_config_t;
+
+extern my_config_t app;
 
 typedef int sbc_status_t;
 
@@ -45,11 +41,21 @@ void app_perror(const char *sender,
                        const char *title,
                        pj_status_t status);
 
+sbc_status_t SBC_init()
+
 /* Determines if Uri is local fo this host */
 pj_bool_t is_uri_local(const pjsip_sip_uri *uri);
 
-pj_status_t SBC_redirect(pjsip_tx_data *tdata);
+/* Changes Request-Uri for redirect */
+sbc_status_t SBC_redirect(pjsip_tx_data *tdata);
 
-extern my_config_t app;
+/* Changes Via header for hide local server */
+pj_status_t via_hdr_hide_host(pjsip_via_hdr *via, pj_str_t host, int port);
+
+/* Changes To header for redirect request/response */
+pj_status_t to_hdr_redirect(pjsip_to_hdr *to, pj_str_t host);
+
+/* Changes From header for hide local server*/
+pj_status_t from_hdr_hide_host(pjsip_from_hdr *from, pj_str_t host);
 
 #endif //__MINI_SBC__
--- src/mini_SBC.c
+++ src/mini_SBC.c
@@ -30,23 +30,26 @@ pj_bool_t is_uri_local(const pjsip_sip_uri *uri)
 sbc_status_t SBC_redirect(pjsip_tx_data *tdata)
 {
     pjsip_sip_uri *target;
-    pjsip_route_hdr *hroute;
+    pjsip_to_hdr *hdr_to;
+    pjsip_from_hdr *hdr_from;
+    pjsip_via_hdr *hdr_via;
 
     target = (pjsip_sip_uri *)tdata->msg->line.req.uri;
-    hroute = (pjsip_route_hdr *)pjsip_msg_find_hdr(tdata->msg, PJSIP_H_ROUTE, NULL);
-    pjsip_msg_find_remove_hdr(tdata->msg,
-                             PJSIP_H_VIA,
-                             NULL);
+
+    hdr_via = (pjsip_via_hdr *)pjsip_msg_find_hdr(tdata->msg, PJSIP_H_VIA, NULL);
+    hdr_to = (pjsip_fromto_hdr *)pjsip_msg_find_hdr(tdata->msg, PJSIP_H_TO, NULL);
+    to_hdr_redirect(hdr_to, pj_str(FIRST_ADDR_FROMTO));
+    hdr_from = (pjsip_fromto_hdr *)pjsip_msg_find_hdr(tdata->msg, PJSIP_H_FROM, NULL);
+    from_hdr_hide_host(hdr_from, pj_str(FIRST_ADDR_FROMTO));
+    //    hroute = (pjsip_route_hdr *)pjsip_msg_find_hdr(tdata->msg, PJSIP_H_ROUTE, NULL);
     if (target->port == app.ports[0])
     {
-        tdata->tp_info.transport = app.trans_port[1];
         char *dest_host = SECOND_ADDR_FROMTO;
         target->host = pj_str(dest_host);
         target->port = 0;
     }
     else if (target->port == app.ports[1])
     {
-        tdata->tp_info.transport = app.trans_port[0];
         char *dest_host = FIRST_ADDR_FROMTO;
         target->host = pj_str(dest_host);
         target->port = 0;
@@ -55,4 +58,68 @@ sbc_status_t SBC_redirect(pjsip_tx_data *tdata)
     {
         return 1;
     }
+}
+
+pj_status_t via_hdr_hide_host(pjsip_via_hdr *via, pj_str_t host, int port)
+{
+    pj_memcpy((void *)&via->sent_by.host, (void *)&host,
+              sizeof(via->sent_by.host));
+    via->sent_by.port = port;
+    via->recvd_param.ptr = "";
+    via->recvd_param.slen = 0;
+}
+
+pj_status_t to_hdr_redirect(pjsip_to_hdr *to, pj_str_t host)
+{
+    pjsip_sip_uri *uri = (pjsip_sip_uri *) pjsip_uri_get_uri(to->uri);
+    uri->host = pj_str(host.ptr);
+}
+
+pj_status_t from_hdr_hide_host(pjsip_from_hdr *from, pj_str_t host)
+{
+    pjsip_sip_uri *uri = (pjsip_sip_uri *) pjsip_uri_get_uri(from->uri);
+    uri->host = pj_str(host.ptr);
+}
+
+static void usage(void)
+{
+    puts("Options:\n"
+         "\n"
+         " -s, --set-listener port<>host(:port)\tSet local listener port to destination host\n");
+}
+
+static pj_status_t init_options(int argc, char *argv[])
+{
+    struct pj_getopt_option long_opt[] = {
+        {"set-port", 1, 0, 'p'},
+        {"help", 0, 0, 'h'},
+        {NULL, 0, 0, 0}};
+    int c;
+    int opt_ind;
+
+    pj_optind = 0;
+    while ((c = pj_getopt_long(argc, argv, "p:h", long_opt, &opt_ind)) != -1)
+    {
+        switch (c)
+        {
+        case 'p':
+            {
+                int local_port, port;
+                char addr[PJ_INET_ADDRSTRLEN];
+                printf(pj_optarg);
+                sscanf(pj_optarg, "%d<>%16s:%d", &local_port, &addr, &port);
+                printf("Port is set to");
+                break;
+            }
+        case 'h':
+            usage();
+            return -1;
+
+        default:
+            puts("Unknown option. Run with --help for help.");
+            return -1;
+        }
+    }
+
+    return PJ_SUCCESS;
 }
